e16368d2dc47c06bbc0c0375a266c4d7e26c4e94
Luiza Santiago <87209516+LuSntgo@users.noreply.github.com>
2022-02-25 14:22:50 -0300

    fix deploy

--- src/services/api.js
+++ src/services/api.js
@@ -6,34 +6,34 @@ function addConfig(token) {
 }
 
 async function signUp(user) {
-  await axios.post(`${REACT_APP_API}/sign-up`, user);
+  await axios.post(`${process.env.REACT_APP_APII}/sign-up`, user);
 }
 async function login(data) {
-  const promise = await axios.post(`${REACT_APP_API}/login`, data);
+  const promise = await axios.post(`${process.env.REACT_APP_API}/login`, data);
   return promise;
 }
 
 async function addContact(data, token) {
   const config = addConfig(token);
 
-  const promise = await axios.post(`${REACT_APP_API}/addContact`, data, config);
+  const promise = await axios.post(`${process.env.REACT_APP_API}/addContact`, data, config);
   return promise;
 }
 
 async function getList(token) {
   const config = addConfig(token);
-  const promise = await axios.get(`${REACT_APP_API}/home`, config);
+  const promise = await axios.get(`${process.env.REACT_APP_API}/home`, config);
   return promise;
 }
 async function deleteContact(id, token) {
   const config = addConfig(token);
 
-  const promise = await axios.delete(`${REACT_APP_API}/home/${id}`, config);
+  const promise = await axios.delete(`${process.env.REACT_APP_API}/home/${id}`, config);
   return promise;
 }
 
 async function updateContact(body, id) {
-  const promise = await axios.put(`${REACT_APP_API}/home/${id}`, body);
+  const promise = await axios.put(`${process.env.REACT_APP_API}/home/${id}`, body);
   return promise;
 }
 

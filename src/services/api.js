60239004c4e9bdeb72b02db3820c82fd05d38876
Luiza Santiago <lu.sntgo@gmail.com>
2022-02-25 00:40:32 -0300

    chore: adjust code for deploy on vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,41 +1,49 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
-
 function addConfig(token) {
   return { headers: { Authorization: `Bearer ${token}` } };
 }
 
 async function signUp(user) {
-  await axios.post(`${BASE_URL}/sign-up`, user);
+  await axios.post(`${process.env.REACT_APP_API}/sign-up`, user);
 }
 async function login(data) {
-  const promise = await axios.post(`${BASE_URL}/login`, data);
+  const promise = await axios.post(`${process.env.REACT_APP_API}/login`, data);
   return promise;
 }
 
 async function addContact(data, token) {
   const config = addConfig(token);
 
-  const promise = await axios.post(`${BASE_URL}/addContact`, data, config);
+  const promise = await axios.post(
+    `${process.env.REACT_APP_API}/addContact`,
+    data,
+    config
+  );
   return promise;
 }
 
 async function getList(token) {
   const config = addConfig(token);
-  const promise = await axios.get(`${BASE_URL}/home`, config);
+  const promise = await axios.get(`${process.env.REACT_APP_API}/home`, config);
   return promise;
 }
 async function deleteContact(id, token) {
   const config = addConfig(token);
 
-  const promise = await axios.delete(`${BASE_URL}/home/${id}`, config);
+  const promise = await axios.delete(
+    `${process.env.REACT_APP_API}/home/${id}`,
+    config
+  );
   return promise;
 }
 
 async function updateContact(body, id) {
   console.log(id);
-  const promise = await axios.put(`${BASE_URL}/home/${id}`, body);
+  const promise = await axios.put(
+    `${process.env.REACT_APP_API}/home/${id}`,
+    body
+  );
   return promise;
 }
 
